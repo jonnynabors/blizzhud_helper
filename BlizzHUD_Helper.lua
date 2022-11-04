7f08a8f311462359802cdaa09e4a9bae1011f782
Jonny Nabors <jonnynabors@gmail.com>
2022-11-04 15:58:23 -0400

    [skip-ci] Refactor to use ACE 3

--- BlizzHUD_Helper.lua
+++ BlizzHUD_Helper.lua
@@ -10,11 +10,9 @@ function BHH:OnInitialize()
     frame:SetScript("OnEvent", function(this, event, ...)
         BHH[event](BHH, ...)
     end)
-    self:Print("Initializing....")
 end
 
 function BHH:ADDON_LOADED()
-    self:Print("addon loaded")
 end
 
 function BHH:OnEnable()
@@ -27,114 +25,54 @@ function BHH:OnEnable()
 end
 
 function LoadPowerBars()
-    -- noOp
+    if ShowHidePowerBars == 'show' then
+        BHH:ShowPowerBars()
+    elseif ShowHidePowerBars == 'hide' then
+        BHH:HidePowerBars()
+    end
 end
 
 function BHH:ProcessSlashCommand(input)
     if (input == "show") then
         ShowHidePowerBars = "show"
+        self:ShowPowerBars()
     elseif (input == "hide") then
         ShowHidePowerBars = "hide"
-        self:HidePalyPower()
+        self:HidePowerBars()
     else
     end
-    self:Print(ShowHidePowerBars)
 end
 
-function BHH:ShowPalyPower()
-    PaladinPowerBarFrame:Show()
+
+function BHH:HidePowerBars()
+    if playerClass == 'Paladin' then
+        PaladinPowerBarFrame:Hide()
+    elseif playerClass == 'Rogue' then
+        ComboPointPlayerFrame:Hide()
+    elseif playerClass == 'Warlock' then
+        WarlockPowerFrame:Hide()
+    elseif playerClass == 'Druid' then
+        ComboPointDruidPlayerFrame:Hide()
+    elseif playerClass == 'Monk' then
+        MonkHarmonyBarFrame:Hide()
+    elseif playerClass == 'Death Knight' then
+        RuneFrame:Hide()
+    end
 end
 
-function BHH:HidePalyPower()
-    self:Print("hiding...")
-    PaladinPowerBarFrame:Hide()
+function BHH:ShowPowerBars()
+    if playerClass == 'Paladin' then
+        PaladinPowerBarFrame:Show()
+    elseif playerClass == 'Rogue' then
+        ComboPointPlayerFrame:Show()
+    elseif playerClass == 'Warlock' then
+        WarlockPowerFrame:Show()
+    elseif playerClass == 'Druid' then
+        ComboPointDruidPlayerFrame:Show()
+    elseif playerClass == 'Monk' then
+        MonkHarmonyBarFrame:Show()
+    elseif playerClass == 'Death Knight' then
+        RuneFrame:Show()
+    end
 end
---- Old stuff below
-
---local frame = CreateFrame('FRAME')
---local name, namespace = ...
---
------ @class BlizzHUD_Helper
---BlizzHUD_Helper = {}
---
------ @type BlizzHUD_Helper
---local BlizzHUD_Helper = BlizzHUD_Helper
---namespace.BlizzHUD_Helper = BlizzHUD_Helper
---
---local function OnEvent(_, event, ...)
---    if event == 'ADDON_LOADED' then
---        if (name == ...) then
---            hooksecurefunc(ClassPowerBar, "Setup", Initialize)
---            hooksecurefunc(ComboPointPlayerFrame, "Setup", Initialize)
---            hooksecurefunc(ComboPointDruidPlayerFrame, "Setup", Initialize)
---            hooksecurefunc(WarlockShardMixin, "Setup", Initialize)
---            hooksecurefunc(MonkHarmonyBarFrame, "Setup", Initialize)
---            hooksecurefunc(DeathKnightResourceOverlayFrame, "Setup", Initialize)
---        end
---    end
---end
---
---frame:HookScript('OnEvent', OnEvent)
---frame:RegisterEvent('ADDON_LOADED')
---
---local playerClass, englishClass = UnitClass("player");
---
---function Initialize()
---    if ShowHidePowerBars == 'show' then
---        ShowPowerBars()
---    elseif ShowHidePowerBars == 'hide' then
---        HidePowerBars()
---    end
---end
---
---function HidePowerBars()
---    if playerClass == 'Paladin' then
---        PaladinPowerBarFrame:Hide()
---    elseif playerClass == 'Rogue' then
---        ComboPointPlayerFrame:Hide()
---    elseif playerClass == 'Warlock' then
---        WarlockPowerFrame:Hide()
---    elseif playerClass == 'Druid' then
---        ComboPointDruidPlayerFrame:Hide()
---    elseif playerClass == 'Monk' then
---        MonkHarmonyBarFrame:Hide()
---    elseif playerClass == 'Death Knight' then
---        RuneFrame:Hide()
---    end
---end
---
---function ShowPowerBars()
---    if playerClass == 'Paladin' then
---        PaladinPowerBarFrame:Show()
---    elseif playerClass == 'Rogue' then
---        ComboPointPlayerFrame:Show()
---    elseif playerClass == 'Warlock' then
---        WarlockPowerFrame:Show()
---    elseif playerClass == 'Druid' then
---        ComboPointDruidPlayerFrame:Show()
---    elseif playerClass == 'Monk' then
---        MonkHarmonyBarFrame:Show()
---    elseif playerClass == 'Death Knight' then
---        RuneFrame:Show()
---    end
---end
---
---SLASH_BLIZZHUD1 = '/bhh';
---function SlashCmdList.BLIZZHUD(msg)
---    if (msg == "show") then
---        ShowHidePowerBars = "show"
---        ShowPowerBars()
---    elseif (msg == "hide") then
---        ShowHidePowerBars = "hide"
---        HidePowerBars()
---    else
---        print("Hi")
---        BlizzHUD_Helper_Frame:Show()
---    end
---end
---
---function BlizzHUD_Helper_OnLoad(something)
---    something.BlizzHUD_Helper_Frame:Show()
---    print("Loading...")
---end
 
